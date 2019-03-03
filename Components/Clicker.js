4cf646b84ee63b9581cc03e558ce330467ed12ff
crossaaron <atcbcrossx@gmail.com>
2019-03-03 11:29:45 -0800

    built basic store/action/dispatch template for Clicker function

--- Components/Clicker.js
+++ Components/Clicker.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 
-export default class Clicker extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            clicks: 0
-        }
-        this.handleClick = this.handleClick.bind(this)
-    }
-
-    handleClick() {
-        this.setState({clicks: this.state.clicks + 1})
-    }
-
+class Clicker extends Component {
     render() {
         return (
             <div>
-                <p>You've clicked this {this.state.clicks} times.</p>
-                <p><button onClick={this.handleClick}>increment</button></p>
+                <p>You've clicked this {this.props.clicks} times.</p>
+                <p><button onClick={this.props.increment}>increment</button></p>
             </div>
         )
-
     }
 }
+
+// how to read from redux
+const mapStateToProps = (state) => {
+    return {
+        clicks: state
+    }
+};
+
+//how to dispatch actions back to redux
+const mapDispatchToProps = (dispatch) => {
+    return {
+        increment: () => dispatch({type: 'INCREMENT'})
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Clicker)
--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from  'react-dom'
-import redux from 'react-redux'
+
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 import App from './Components/App'
 import './style.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const counterReducer = (state=0, action) => {
+    switch (action.type) {
+        case 'INCREMENT': return state + 1;
+        default: return state
+    }
+};
+
+const store = createStore(counterReducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
